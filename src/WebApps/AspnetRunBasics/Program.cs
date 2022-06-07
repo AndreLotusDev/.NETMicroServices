f9d14892a6ef22391909e7a65181c13deba1dce5
Andre <andrsoares953@yahoo.com>
2022-06-07 09:41:10 -0300

    Finished to configure serilog for elastic search in the program.cs

--- src/WebApps/AspnetRunBasics/Program.cs
+++ src/WebApps/AspnetRunBasics/Program.cs
@@ -1,5 +1,9 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+using System;
+using System.Reflection;
 
 namespace AspnetRunBasics
 {
@@ -12,6 +16,23 @@ namespace AspnetRunBasics
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog((context, config) =>
+                {
+                    config
+                            .Enrich.WithMachineName()
+                            .Enrich.FromLogContext()
+                            .Enrich.WithProperty("Enviroment", context.HostingEnvironment.EnvironmentName)
+                            .WriteTo.Elasticsearch(
+                                new ElasticsearchSinkOptions(new Uri(context.Configuration["ElasticConfigs:Uri"]))
+                                {
+                                    IndexFormat = $"applogs-{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".", "-")}-{context.HostingEnvironment.EnvironmentName?.ToLower().Replace(".", "-")}-logs-{DateTime.UtcNow}",
+                                    AutoRegisterTemplate = true,
+                                    NumberOfShards = 2,
+                                    NumberOfReplicas = 1
+                                }
+                            )
+                            .ReadFrom.Configuration(context.Configuration);
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
